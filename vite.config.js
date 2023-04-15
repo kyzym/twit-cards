1f5e66390bb702e45de03dd5a6a99b58f49b264a
kyzym <kyzym1@gmail.com>
2023-04-15 14:18:36 +0200

    fix: deploy 4 04/15/2023 14:18:36

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/twit-cards/" : "/",
+  base: "/twit-cards/",
   build: {
     outDir: "dist",
-    assetsDir: "",
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
+    assetsDir: "assets",
+    publicPath: "/twit-cards/",
   },
 });
