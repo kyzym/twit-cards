12cef93badea776a196ff653c7c7ac1c4933cf4f
kyzym <kyzym1@gmail.com>
2023-04-15 18:28:07 +0200

    fix: homepage text 04/15/2023 18:28:07

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { StyledHomePageWrapper } from "./HomePage.styled";
 
 export const Home = () => {
   return (
-    <div>
+    <StyledHomePageWrapper>
       <h1>Welcome!</h1>
       <h2>
         To see the latest tweets, please <br /> click the "Tweets" button
         <br /> at the top-left corner of the page.
       </h2>
-    </div>
+    </StyledHomePageWrapper>
   );
 };
